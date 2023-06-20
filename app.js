68cc8cd3dc1b22f1fafd44ea56d305c2dd1c1153
Moemen <moemensaied13@gmail.com>
2023-06-20 13:00:33 +0100

    app.js

--- app.js
+++ app.js
@@ -24,12 +24,6 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
 }
 
-app.use(express.static(path.join(__dirname,"../frontend/build")))
-
-app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,"../frontend/build/index.html"))
-})
-
 
 
 const port = process.env.PORT || 8081;
